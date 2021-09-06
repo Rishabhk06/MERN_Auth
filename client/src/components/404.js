46ecdb695ec60d279e93a9c3d3016ae16ba40fe2
Aman Kalra <amankalra@Amans-MacBook-Pro.local>
2021-09-06 12:55:44 +0530

    added 404 component

---
+++ client/src/components/404.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Page404 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>PAGE NOT FOUND</h1>
+      </div>
+    );
+  }
+}
+
+export default Page404;
