74374478d405fe7d9e12a9d45e5e25188f98a678
Aman Kalra <amankalra@Amans-MacBook-Pro.local>
2021-09-10 16:08:53 +0530

    config heroku

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, console.log(`Server started at port ${port}`));
-
 //Setting Mongo connection
 const atlasURL =
   "mongodb+srv://rishabh:rishabh@users.xclt4.mongodb.net/users?retryWrites=true&w=majority";
@@ -73,4 +69,15 @@ app.get(
 //   console.log("deleted all");
 // });
 
-console.log(process.env.NODE_ENV);
+//Setup heroku
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile("client/build/index.html ");
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`Server started at port ${port}`));
