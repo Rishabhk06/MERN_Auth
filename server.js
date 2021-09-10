03ae9c01c84da383c1c51f67af2e351b0eaa18ed
Aman Kalra <amankalra@Amans-MacBook-Pro.local>
2021-09-10 19:07:17 +0530

    heroku

--- server.js
+++ server.js
@@ -80,4 +80,4 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`Server started at port ${port}`));
+app.listen(port, () => console.log(`Server started at port ${port}`));
