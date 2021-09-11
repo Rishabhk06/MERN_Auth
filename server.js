7c5fcc49277c729a9f280b7ea91dacee0199e495
Aman Kalra <amankalra@Amans-MacBook-Pro.local>
2021-09-11 20:20:49 +0530

    debugging dashboard for heroku

--- server.js
+++ server.js
@@ -78,6 +78,9 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+  app.get("/dashboard", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 5000;
